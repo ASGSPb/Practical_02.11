16cd9c67c72569637041fdccd1fd235aa2ad7c9a
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-12-04 13:04:30 +0300

    final version

---
+++ Task02/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт
+//  сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.WriteLine("Введите интервал ограниченный значениями M и N");
+Console.WriteLine();
+
+Console.Write("Введите M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+
+Console.WriteLine("Сумма элементов указанного промежутка равна = " + CountingInterval(n,m));
+Console.WriteLine();
+
+
+
+int CountingInterval(int n, int m)
+{
+ int result = 0;
+ for (int i = m; i <= n; i++) result += i;
+ return result;
+}
+int SumRec(int n)
+{
+ if (n == 0) return 0;
+ else return n + SumRec(n - 1);
+}
+
---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+Console.Write("Введите начальное число M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите начальное число N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int AckFunction (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return AckFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckFunction(m - 1, AckFunction(m, n - 1));
+return AckFunction(m, n);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел ({m},{n}) будет ровна = {AckFunction(m, n)}");
+Console.WriteLine();
